a2acc7f4ce579d9e9f05ceccafb41b6f4fb7afa5
Frouzin <matheusereis44@gmail.com>
2023-07-07 12:05:06 -0300

    Aplicando Orientação a Objetos no codigo

--- hangman.py
+++ hangman.py
@@ -1,51 +1,49 @@
 import random
+from constants import DIFFICULTY_LEVELS, WORDS_LENGTH
 
-difficulty_levels = {
-    "1": "facil",
-    "2": "normal",
-    "3": "dificil"
-}
+def show_difficulty_menu():
+    """MENU DE DIFICULDADE"""
+    print("Escolha o Nivel de Dificuldade:")
+    difficulty_settings = ""
 
-words_length = {
-    "1" : (3, 5),
-    "2" : (6, 10),
-    "3" : (11, 100)
-}
+    while not difficulty_settings:
+        
+        for k,v in DIFFICULTY_LEVELS.items():
+            print(f"{k} - {v.upper()}")
 
-print("Escolha o Nivel de Dificuldade:")
-difficulty_settings = ""
+        difficulty_settings = input("Escolha sua dificuldade:")
+        
+        if difficulty_settings not in DIFFICULTY_LEVELS.keys():
+            print(f"{difficulty_settings} nao é uma opção valida")
+            difficulty_settings = ""
 
-while not difficulty_settings:
-    
-    for k,v in difficulty_levels.items():
-        print(f"{k} - {v.upper()}")
+    return difficulty_settings
 
-    difficulty_settings = input("Escolha sua dificuldade:")
-    
-    if difficulty_settings not in difficulty_levels.keys():
-        print(f"{difficulty_settings} nao é uma opção valida")
-        difficulty_settings = ""
+def get_random_word(difficulty_settings):
+    """SELECIONA UMA PALAVRA ALEATORIA EM RELAÇÃO AO NIVEL DE DIFICULDADE"""
+    with open("static\words.txt", mode="r") as f_words:
+        words = []
+        for word in f_words.readlines():
+            w = word.strip()
+            min,max = WORDS_LENGTH[difficulty_settings]
+
+            if min <= len(w) <= max:
+                words.append(w)
+
+    max_index = len(words)-1
+    random_index = random.randint(0,max_index)
+    selected_word = words[random_index]
+
+    print(selected_word)
+    return selected_word
 
 
-with open("static\words.txt", mode="r") as f_words:
-    words = []
-    for word in f_words.readlines():
-        w = word.strip()
-        min,max = words_length[difficulty_settings]
+def get_total_tries(selected_word, difficulty_settings):
+    """EXIBE O TOTAL DE TENTATIVAS PARA RESOLVER O PROBLEMA"""
+    total_tries = 2* len(selected_word)
+    if difficulty_settings == "1":
+        total_tries += 2
+    elif difficulty_settings == "3":
+        total_tries -= 2
 
-        if min <= len(w) <= max:
-            words.append(w)
-
-max_index = len(words)-1
-random_index = random.randint(0,max_index)
-selected_word = words[random_index]
-
-print(selected_word)
-
-total_tries = 2* len(selected_word)
-if difficulty_settings == "1":
-    total_tries += 2
-elif difficulty_settings == "3":
-    total_tries -= 2
-
-print(f"A quantidade de chance para advinhar a palavra é: {total_tries}")
+    print(f"A quantidade de chance para advinhar a palavra é: {total_tries}")
