366090e886de2207ea3e06deda7b11ceed536eec
Frouzin <matheusereis44@gmail.com>
2023-07-07 10:50:36 -0300

    palavra com base na dificuldade

--- hangman.py
+++ hangman.py
@@ -6,23 +6,38 @@ difficulty_levels = {
     "3": "dificil"
 }
 
+words_length = {
+    "1" : (3, 5),
+    "2" : (6, 10),
+    "3" : (11, 100)
+}
+
 print("Escolha o Nivel de Dificuldade:")
 difficulty_settings = ""
+
 while not difficulty_settings:
+    
     for k,v in difficulty_levels.items():
         print(f"{k} - {v.upper()}")
 
     difficulty_settings = input("Escolha sua dificuldade:")
     
     if difficulty_settings not in difficulty_levels.keys():
+        print(f"{difficulty_settings} nao é uma opção valida")
         difficulty_settings = ""
 
 
 with open("static\words.txt", mode="r") as f_words:
     words = []
-    for words in f_words.readlines():
-        words.append(words.strip())
+    for word in f_words.readlines():
+        w = word.strip()
+        min,max = words_length[difficulty_settings]
+
+        if min < len(w) < max:
+            words.append(w)
 
 max_index = len(words)-1
 random_index = random.randint(0,max_index)
 selected_word = words[random_index]
+
+print(selected_word)
